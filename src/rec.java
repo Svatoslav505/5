6e5be969192c4a734bbd3ff0f967a616992cde51
shinz99 <38512755+shinz99@users.noreply.github.com>
2020-10-03 11:30:42 +0300

    first commit

---
+++ src/rec.java
@@ -0,0 +1,22 @@
+public class rec {
+
+        public static void recursion(int n, int k) {
+
+            if (k > n / 2) {
+                System.out.println(n);
+                return;
+            }
+
+            if (n % k == 0) {
+                System.out.println(k);
+                recursion(n / k, k);
+            }
+            else {
+                recursion(n, ++k);
+            }
+        }
+        public static void main(String[] args) {
+            recursion(150, 2);
+        }
+
+}
